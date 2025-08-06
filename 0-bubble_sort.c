5132dbbdc8582c75f051b73d0c120ff4405d8a93
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-06 09:10:49 +0200

    Implement bubble sort algorithm with array printing

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,5 +8,27 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-
+    int i, j, tmp;
+    
+    i = 0;
+    if (array == NULL || size < 2)
+        return;
+    
+    while (i < (int)size - 1)
+    {
+        j = 0;
+        while (j < (int)size - i - 1)
+        {
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                print_array(array, size);
+            }
+            j++;
+        }
+        i++;
+    }
+    print_array(array, size);
 }
