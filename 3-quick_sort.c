3dd959d89451dcef7b9a0deafd26818ad9bd809e
JordannM83 <11457@holbertonstudents.com>
2025-08-06 14:54:38 +0200

    Implement quick sort algorithm with Lomuto partition scheme and add array printing functionality

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,12 +1,84 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * partition - Partitions the array using the Lomuto partition scheme
+ * @array: The array to partition
+ * @low: The starting index
+ * @high: The ending index
+ * @size: Size of the array (for printing)
+ * Return: The index of the pivot element after partitioning
+ */
+
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (i + 1 != high)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quick_sort_recursive - Recursively sorts an array
+ * using the Quick sort algorithm
+ * @array: The array to be sorted
+ * @low: The starting index
+ * @high: The ending index
+ * @size: Size of the array (for printing)
+ */
+
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot_index = partition(array, low, high, size);
+
+		quick_sort_recursive(array, low, pivot_index - 1, size);
+		quick_sort_recursive(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
  * quick_sort - Sorts an array of integers in ascending order using
  *              the Quick sort algorithm (Lomuto partition scheme)
  * @array: The array to be sorted
  * @size: Number of elements in @array
  */
+
 void quick_sort(int *array, size_t size)
 {
+	if (array == NULL || size < 2)
+		return;
 
+	quick_sort_recursive(array, 0, size - 1, size);
 }
